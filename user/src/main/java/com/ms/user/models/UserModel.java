c37b30f20c57b3324f0f162b48eda9a131e12cb1
Gabriel <shieldhero0109@gmail.com>
2023-10-23 16:44:40 -0300

    initial commit

---
+++ user/src/main/java/com/ms/user/models/UserModel.java
@@ -0,0 +1,26 @@
+package com.ms.user.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TB_USERS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+}
